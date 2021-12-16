0245444e31e8cb6dd583cd76156771bc397b5acb
jtpaplham <74616892+jtpaplham@users.noreply.github.com>
2021-12-16 12:58:41 -0600

    Added App component with Welcome placeholder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./app";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hi</h1>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
